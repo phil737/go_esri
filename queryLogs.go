9b909ea7d4aec6a24410209e6b33eca086816ec0
Philippe Waecker <waecker@posteo.me>
2021-12-09 11:45:31 +0100

    added endTime parameter to QueryLogs func

--- queryLogs.go
+++ queryLogs.go
@@ -7,6 +7,7 @@ package go_esri
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/url"
 
 	"github.com/go-resty/resty/v2"
@@ -29,7 +30,7 @@ type logResponseJSON struct {
 }
 
 // query ArcGIS server logs, only records with a log level at or more severe than this (SEVERE, WARNING, INFO, FINE, VERBOSE, DEBUG).
-func QueryLogs(token, serverName, levelType string) (*logResponseJSON, error) {
+func QueryLogs(token, serverName, levelType string, endT int64) (*logResponseJSON, error) {
 
 	// ----------------------------------------- build and validate url
 	baseUrl, err := url.Parse(serverName)
@@ -43,7 +44,7 @@ func QueryLogs(token, serverName, levelType string) (*logResponseJSON, error) {
 	v := url.Values{}
 	v.Set("token", token)
 	v.Add("startTime", "") // 2021-12-07T00:55:23
-	v.Add("endTime", "")
+	v.Add("endTime", fmt.Sprint(endT))
 	v.Add("level", levelType)
 	v.Add("filterType", "json")
 	v.Add("pageSize", "100")
